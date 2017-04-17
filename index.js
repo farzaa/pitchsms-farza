5252615ea181df6d410e29dd475c252b79cfc2e1
evanfrawley <frawley@uw.edu>
2017-04-17 00:51:50 -0700

    test

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
+//
+// var createMsg = require('./createMsgFromZip.js');
+//
+// var accountSid = 'AC87b63e9e453fc85755fa9a7271117763'; // Your Account SID from www.twilio.com/console
+// var authToken = '510cd92cc4d94598f4f2ded16b1f282f';   // Your Auth Token from www.twilio.com/console
+//
+// var twilio = require('twilio');
+// var client = new twilio.RestClient(accountSid, authToken);
+//
+// app.set('port', (process.env.PORT || 5000));
+//
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+// 
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+//
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+//
+// app.post('/sms', function(request, response) {
+//   var zip = request.body.Body;
+//   var msg = createMsg(zip);
+//   var twiml = new twilio.TwimlResponse();
+//   twiml.message(msg);
+//   res.writeHead(200, {'Content-Type': 'text/xml'});
+//   res.end(twiml.toString());
+// });
+
+var http = require('http');
 var express = require('express');
-var bodyParser = require('body-parser');
+var twilio = require('twilio');
+
 var app = express();
 
-var createMsg = require('./createMsgFromZip.js');
-
-var accountSid = 'AC87b63e9e453fc85755fa9a7271117763'; // Your Account SID from www.twilio.com/console
-var authToken = '510cd92cc4d94598f4f2ded16b1f282f';   // Your Auth Token from www.twilio.com/console
-
-var twilio = require('twilio');
-var client = new twilio.RestClient(accountSid, authToken);
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-app.post('/sms', function(request, response) {
-  var zip = request.body.Body;
-  var msg = createMsg(zip);
+app.post('/sms', function(req, res) {
+  var twilio = require('twilio');
   var twiml = new twilio.TwimlResponse();
-  twiml.message(msg);
+  twiml.message('The Robots are coming! Head for the hills!');
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, function () {
+  console.log("Express server listening on port 1337");
 });
 // TEST CASES
 // let zip = '91108';
